e501ddf4fe384ab3568fee9d397cd259b1a1a82a
netravler <pheintz@hotmail.com>
2018-09-13 13:30:16 -0500

    Add files via upload

---
+++ quickCreateWinForm/Form1.Designer.cs
@@ -0,0 +1,126 @@
+﻿namespace quickCreateWinForm
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.createFromToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.textBoxToolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.richtextBoxToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.listBoxToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.checkBoxToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.createFromToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(656, 28);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // createFromToolStripMenuItem
+            // 
+            this.createFromToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.textBoxToolStripMenuItem1,
+            this.richtextBoxToolStripMenuItem,
+            this.listBoxToolStripMenuItem,
+            this.checkBoxToolStripMenuItem,
+            this.exitToolStripMenuItem});
+            this.createFromToolStripMenuItem.Name = "createFromToolStripMenuItem";
+            this.createFromToolStripMenuItem.Size = new System.Drawing.Size(102, 24);
+            this.createFromToolStripMenuItem.Text = "Create From";
+            // 
+            // textBoxToolStripMenuItem1
+            // 
+            this.textBoxToolStripMenuItem1.Name = "textBoxToolStripMenuItem1";
+            this.textBoxToolStripMenuItem1.Size = new System.Drawing.Size(156, 24);
+            this.textBoxToolStripMenuItem1.Text = "TextBox";
+            this.textBoxToolStripMenuItem1.Click += new System.EventHandler(this.textBoxToolStripMenuItem1_Click);
+            // 
+            // richtextBoxToolStripMenuItem
+            // 
+            this.richtextBoxToolStripMenuItem.Name = "richtextBoxToolStripMenuItem";
+            this.richtextBoxToolStripMenuItem.Size = new System.Drawing.Size(156, 24);
+            this.richtextBoxToolStripMenuItem.Text = "RichtextBox";
+            this.richtextBoxToolStripMenuItem.Click += new System.EventHandler(this.richtextBoxToolStripMenuItem_Click);
+            // 
+            // listBoxToolStripMenuItem
+            // 
+            this.listBoxToolStripMenuItem.Name = "listBoxToolStripMenuItem";
+            this.listBoxToolStripMenuItem.Size = new System.Drawing.Size(156, 24);
+            this.listBoxToolStripMenuItem.Text = "ListBox";
+            this.listBoxToolStripMenuItem.Click += new System.EventHandler(this.listBoxToolStripMenuItem_Click);
+            // 
+            // checkBoxToolStripMenuItem
+            // 
+            this.checkBoxToolStripMenuItem.Name = "checkBoxToolStripMenuItem";
+            this.checkBoxToolStripMenuItem.Size = new System.Drawing.Size(156, 24);
+            this.checkBoxToolStripMenuItem.Text = "CheckBox";
+            this.checkBoxToolStripMenuItem.Click += new System.EventHandler(this.checkBoxToolStripMenuItem_Click);
+            // 
+            // exitToolStripMenuItem
+            // 
+            this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+            this.exitToolStripMenuItem.Size = new System.Drawing.Size(156, 24);
+            this.exitToolStripMenuItem.Text = "Exit";
+            this.exitToolStripMenuItem.Click += new System.EventHandler(this.exitToolStripMenuItem_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(656, 292);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Quick Create WinForm Demo";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem createFromToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem textBoxToolStripMenuItem1;
+        private System.Windows.Forms.ToolStripMenuItem richtextBoxToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem listBoxToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem checkBoxToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
+    }
+}
+
---
+++ quickCreateWinForm/Form1.cs
@@ -0,0 +1,178 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace quickCreateWinForm
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBoxToolStripMenuItem1_Click(object sender, EventArgs e)
+        {
+            using (Form formNew = new Form())
+            {
+                formNew.Text = "Created Form";
+                formNew.Size = new System.Drawing.Size(400, 200);
+
+                addtxtBox("TextBox", 10, 25, 150, 20, formNew);
+
+                formNew.ShowDialog();
+             }
+        }
+
+        private void richtextBoxToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            using (Form formNew = new Form())
+            {
+                formNew.Text = "Created Form";
+                formNew.Size = new System.Drawing.Size(400, 200);
+
+                addrichTextBox("RichTextBox", 10, 25, 150, 100, formNew);
+
+                formNew.ShowDialog();
+            }
+        }
+
+        private void listBoxToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            using (Form formNew = new Form())
+            {
+                formNew.Text = "Created Form";
+                formNew.Size = new System.Drawing.Size(400, 200);
+
+                addListBox("ListBox", 10, 20, 150, 20, formNew);
+
+                formNew.ShowDialog();
+            }
+        }
+
+        private void checkBoxToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            using (Form formNew = new Form())
+            {
+                formNew.Text = "Created Form";
+                formNew.Size = new System.Drawing.Size(400, 200);
+
+                addCheckBox("CheckBox", 10, 25, 150, 20, formNew);
+
+                formNew.ShowDialog();
+            }
+        }
+
+        private void exitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            this.Dispose();
+        }
+
+        private void addtxtBox(string txtBoxName, int locationL, int locationT, int sizeL, int sizeLen, Form parentForm)
+        {
+            TextBox txtExt1 = new TextBox();
+            {
+                txtExt1.Name = txtBoxName.ToString();
+                txtExt1.Text = txtBoxName.ToString();
+                txtExt1.Location = new System.Drawing.Point(locationL, locationT);
+                txtExt1.Size = new System.Drawing.Size(sizeL, sizeLen);
+
+                //Hover
+                txtExt1.MouseHover += delegate(object sender, EventArgs e)
+                {
+                    MessageBox.Show("this is a Hover " + txtExt1.Name);
+                };
+
+                //Click
+                txtExt1.Click += delegate(object sender, EventArgs e)
+                {
+                    MessageBox.Show("this is a Click " + txtExt1.Name);
+                };
+
+                parentForm.Controls.Add(txtExt1);
+            }
+        }
+
+        private void addrichTextBox(string txtBoxName, int locationL, int locationT, int sizeL, int sizeLen, Form parentForm)
+        {
+            RichTextBox RTBox1 = new RichTextBox();
+            {
+                RTBox1.Name = txtBoxName.ToString();
+                RTBox1.Text = txtBoxName.ToString();
+                RTBox1.Location = new System.Drawing.Point(locationL, locationT);
+                RTBox1.Size = new System.Drawing.Size(sizeL, sizeLen);
+
+                //Hover
+                RTBox1.MouseHover += delegate(object sender, EventArgs e)
+                {
+                    MessageBox.Show("this is a Hover " + RTBox1.Name);
+                };
+
+                //Click
+                RTBox1.Click += delegate(object sender, EventArgs e)
+                {
+                    MessageBox.Show("this is a Click " + RTBox1.Name);
+                };
+
+                parentForm.Controls.Add(RTBox1);
+            }
+        }
+
+        private void addListBox(string listboxname, int locationL, int locationT, int sizeL, int sizeLen, Form parentForm)
+        {
+            ListBox LSTBox1 = new ListBox();
+            {
+                LSTBox1.Name = listboxname.ToString();
+                LSTBox1.Text = listboxname.ToString();
+                LSTBox1.Location = new System.Drawing.Point(locationL, locationT);
+                LSTBox1.Size = new System.Drawing.Size(sizeL, sizeLen);
+
+                //Hover
+                LSTBox1.MouseHover += delegate(object sender, EventArgs e)
+                {
+                    MessageBox.Show("this is a Hover " + LSTBox1.Name);
+                };
+
+                //Click
+                LSTBox1.Click += delegate(object sender, EventArgs e)
+                {
+                    MessageBox.Show("this is a Click " + LSTBox1.Name);
+                };
+
+                parentForm.Controls.Add(LSTBox1);
+            }
+        }
+
+        private void addCheckBox(string checkboxname, int locationL, int locationT, int sizeL, int sizeLen, Form parentForm)
+        {
+            CheckBox CKBox1 = new CheckBox();
+            {
+                CKBox1.Name = checkboxname.ToString();
+                CKBox1.Text = checkboxname.ToString();
+                CKBox1.Location = new System.Drawing.Point(locationL, locationT);
+                CKBox1.Size = new System.Drawing.Size(sizeL, sizeLen);
+
+                //Hover
+                CKBox1.MouseHover += delegate(object sender, EventArgs e)
+                {
+                    MessageBox.Show("this is a Hover " + CKBox1.Name);
+                };
+
+                parentForm.Controls.Add(CKBox1);
+            }
+        }
+
+     
+
+    }
+}
